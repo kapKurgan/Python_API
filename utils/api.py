0534093a1e5f969d86960bb0ae477d8c177d870b
kkggaa45 <kkggaa45@yandex.ru>
2025-11-22 21:47:31 +0500

    Создать класс GoogleMapsApi

---
+++ utils/api.py
@@ -0,0 +1,38 @@
+from http_methods import HttpMethods                        # импорт класса HttpMethods
+
+BASE_URL = "https://rahulshettyacademy.com"
+KEY = "?key=qaclick123"
+
+
+class GoogleMapsApi():
+    """Класс содержащий методы, для тестирования Google maps api"""
+
+    @staticmethod
+    def create_new_place():
+        """Метод по созданию новой локации"""
+
+        json_for_create_new_place = {
+            "location": {
+                "lat": -38.383494,
+                "lng": 33.427362
+            }, "accuracy": 50,
+            "name": "Frontline house",
+            "phone_number": "(+91) 983 893 3937",
+            "address": "29, side layout, cohen 09",
+            "types": [
+                "shoe park",
+                "shop"
+            ],
+            "website": "http://google.com",
+            "language": "French-IN"
+        }
+
+        POST_RESOURCE = "/maps/api/place/add/json"  # ресурс метода POST
+        post_url = BASE_URL + POST_RESOURCE + KEY
+        print(post_url)
+        result_post = HttpMethods.post(post_url, json_for_create_new_place)  # отправка метода POST, в котором указываем url и body
+        print(result_post.json())
+        print(result_post.status_code)
+        return result_post
+
+
--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,28 +1,28 @@
 import requests
 
 
-class Http_methods:
+class HttpMethods:
     """Класс по хранению HTTP методов"""
 
     headers = {'Content-type': 'application/json'}  # Заголовки проекта передавать в формате json
     cookie = ""                                     # Куки проекта
 
-    @staticmethod   # сделать метод статичным, чтобы вызывать откуда угодно
+    @staticmethod   # сделать метод статичным, чтобы вызывать его откуда угодно
     def get(url):
-        result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
         return result
 
     @staticmethod
     def post(url, body):
-        result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
         return result
 
     @staticmethod
     def put(url, body):
-        result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
         return result
 
     @staticmethod
     def delete(url, body):
-        result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
         return result
