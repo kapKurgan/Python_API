854d8d1539371d78003c2fd9a741b82c2dc7a0a7
kkggaa45 <kkggaa45@yandex.ru>
2025-11-23 20:58:06 +0500

    Проверка наличия обязательных полей

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,4 +1,5 @@
 #  pytest -v -s tests\test_google_maps_api.py
+import json
 
 from ..utils.api import GoogleMapsApi
 from ..utils.checking import Checking
@@ -14,6 +15,7 @@ class TestCreatePlace():
         print("\n>>>>>>>>>>>>>   Метод POST   <<<<<<<<<<<<<<<<<<")
         result_post = GoogleMapsApi.create_new_place()          # вызов метода по созданию новой локации
         Checking.check_status_code(result_post, 200)
+        Checking.check_json_fields(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
 
         check_post = result_post.json()
         place_id = check_post.get("place_id")                   # получения place_id для метода GET
@@ -21,21 +23,28 @@ class TestCreatePlace():
         print(">>>>>>>>>>>>>   Метод GET POST")
         result_get = GoogleMapsApi.get_new_place(place_id)      # отправка метода Get
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_fields(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
 
         print(">>>>>>>>>>>>>   Метод PUT")
         result_put = GoogleMapsApi.put_new_place(place_id)  # изменение данных о созданной локации
         Checking.check_status_code(result_put, 200)
+        Checking.check_json_fields(result_put, ['msg'])
 
         print(">>>>>>>>>>>>>   Метод GET PUT")
         result_get = GoogleMapsApi.get_new_place(place_id)  # отправка метода Get
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_fields(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
 
         print(">>>>>>>>>>>>>   Метод DELETE")
         result_delete = GoogleMapsApi.delete_new_place(place_id)  # удаление данных о созданной локации
         Checking.check_status_code(result_delete, 200)
+        Checking.check_json_fields(result_delete, ['status'])
+        # token = result_delete.json()                             # получить все поля в json из ответа
+        # print("token =", token)
 
         print(">>>>>>>>>>>>>   Метод GET DELETE")
         result_get = GoogleMapsApi.get_new_place(place_id)  # отправка метода Get
         Checking.check_status_code(result_get, 404)
+        Checking.check_json_fields(result_get, ['msg'])
 
 
--- utils/checking.py
+++ utils/checking.py
@@ -1,3 +1,4 @@
+import json
 
 
 class Checking():
@@ -6,4 +7,12 @@ class Checking():
     def check_status_code(result, status_code):
         """Метод для проверки статус кода"""
         assert status_code == result.status_code, 'ОШИБКА, Статус-код не совпадает'
-        print(f"Успешно! Статус код = {result.status_code}")
+        print(f"--->  Успешно! Статус код = {result.status_code}")
+
+    @staticmethod
+    def check_json_fields(result, expected_value):
+        """Метод для проверки наличия полей в ответе запроса"""
+        fields = json.loads(result.text)
+        assert list(fields) == expected_value, 'ОШИБКА, Список полей не совпадает'
+        print(list(fields))
+        print("--->  Все поля присутствуют")
